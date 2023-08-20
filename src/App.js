6829e8d76ce3b6aea7dd81571017990f1bb324e1
Ayush Jha <ayushkumarjha666@gmil.com>
2023-08-21 02:53:32 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React, { useEffect, useState } from 'react'
 
-function App() {
+const App = () => {
+
+  const [endPoint, setEndPoint] = useState('');
+  const [container, setContainer] = useState([]);
+  const [finalPoint, setFinalPoint] = useState('');
+
+  useEffect(() => {
+    fetchMe();
+  }, [finalPoint]);
+
+
+  const fetchMe = () => {
+
+    fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q=+${endPoint}`, {
+      "method": "GET",
+      "headers": {
+        'X-RapidAPI-Key': 'a1a3c0bd7cmsh48aefb17e399b66p17b261jsnfb411f170ae0',
+        'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+      }
+    })
+
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setContainer(data.d);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+
+  }
+  const changeHandle = (e) => {
+    setEndPoint(e.target.value);
+  }
+
+  const preventSubmit = (e) => {
+    e.preventDefault();
+    setFinalPoint(endPoint);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={preventSubmit}>
+        <input type="text" value={endPoint} onChange={changeHandle} />
+        <button type='submit'>Submit</button>
+      </form>
+
+      <div className="element">
+        {container.map((item, ind) => {
+          return (
+            <div key={ind} className="element-div">
+              <img src={item.i.imageUrl} alt="" />
+              <p>{item.l}</p>
+              <h3>{item.s}</h3>
+            </div>
+          )
+        })}
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
